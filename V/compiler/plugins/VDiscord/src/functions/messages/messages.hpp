66f755003f88ad154856c3ed19002406b3dadffc
BaranBey1331 <152785244+BaranBey1331@users.noreply.github.com>
2026-03-01 18:15:19 +0300

    Clean up messages.hpp by removing comments
    Removed comments and unnecessary lines from messages.hpp.

--- V/compiler/plugins/VDiscord/src/functions/messages/messages.hpp
+++ V/compiler/plugins/VDiscord/src/functions/messages/messages.hpp
@@ -1,7 +1,5 @@
-#pragma once // Bu dosyanın belleğe iki kere yüklenmesini engeller
+#pragma once
 
 namespace VDiscordAPI {
-    // English-first, zero-delay function declarations
     void sendMessage(const char* channel_id, const char* content);
-    void sendEmbed(const char* channel_id, const char* title, const char* description, int hex_color);
 }
